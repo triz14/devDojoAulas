314c85577e9b495754622f46ff386e720edc90fe
Beatriz <beatriznovais1404@gmail.com>
2025-07-01 22:40:02 -0300

    Aula 54, modificadores de acesso e getters e setters

---
+++ src/aula_54/PessoaTeste01.java
@@ -0,0 +1,11 @@
+package aula_54;
+
+public class PessoaTeste01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Goku");
+        pessoa.setIdade(23);
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+    }
+}
