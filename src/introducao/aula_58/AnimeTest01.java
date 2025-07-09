918103103b781708dbe4e5b7fa29dfc8c4afa755
Beatriz <beatriznovais1404@gmail.com>
2025-07-08 21:49:31 -0300

    Aula 58, métodos construtores

---
+++ src/introducao/aula_58/AnimeTest01.java
@@ -0,0 +1,8 @@
+package introducao.aula_58;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dragon Ball", "Ação", 120, "Fantasia");
+        anime.imprime();
+    }
+}
